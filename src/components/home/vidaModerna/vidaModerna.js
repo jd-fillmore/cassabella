004d70d764d60507e76c0dc427addaa4c6dfb764
JD <jd@infused.agency>
2022-01-15 16:11:23 -0500

    Create Vida Moderna component

---
+++ src/components/home/vidaModerna/vidaModerna.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { Container, Col, Row } from "reactstrap"
+
+import CardLink from "../../cardLink/cardLink"
+import Aquavida from "../../../images/aquavida.jpg"
+import Europa from "../../../images/europa.jpg"
+
+import "./vida-moderna.scss"
+
+const VidaModerna = ({ title, subTitle, desc }) => {
+  return (
+    <>
+      <section className="vida-moderna text-center">
+        <Container>
+          <Row>
+            <Col lg="12">
+              <h2>{title}</h2>
+              <h4>{subTitle}</h4>
+              <p>{desc}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg="6">
+              <CardLink
+                image={Aquavida}
+                imageAlt="Aquavida"
+                link="/vida-moderna#aquavida"
+                btnText="Aquavida"
+              />
+            </Col>
+            <Col lg="6">
+              <CardLink
+                image={Europa}
+                imageAlt="Europa"
+                link="/vida-moderna#europa"
+                btnText="Europa"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  )
+}
+
+export default VidaModerna
